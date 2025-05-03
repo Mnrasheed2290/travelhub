a8dedccc1c5ac2c43bb60395109701bcc7f146fc
Mnrasheed2290 <naim.maria2@gmail.com>
2025-05-03 12:07:01 -0500

    addedallapisUpdate index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,28 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./index.css";
+const API_KEYS = {
+  flight: {
+    key: process.env.REACT_APP_flightAMADEUS_API_KEY,
+    secret: process.env.REACT_APP_flightAMADEUS_API_SECRET,
+  },
+  hotelSearch: {
+    key: process.env.REACT_APP_hotelsearchAMADEUS_API_KEY,
+    secret: process.env.REACT_APP_hotelsearchAMADEUS_API_SECRET,
+  },
+  hotelBooking: {
+    key: process.env.REACT_APP_hotelbookingAMADEUS_API_KEY,
+    secret: process.env.REACT_APP_hotelbookingAMADEUS_API_SECRET,
+  },
+  flightOfferSearch: {
+    key: process.env.REACT_APP_flightoffersearchAMADEUS_API_KEY,
+    secret: process.env.REACT_APP_flightoffersearchAMADEUS_API_SECRET,
+  },
+  flightOfferPrice: {
+    key: process.env.REACT_APP_flightofferpriceAMADEUS_API_KEY,
+    secret: process.env.REACT_APP_flightofferpriceAMADEUS_API_SECRET,
+  },
+  flightCreateOrder: {
+    key: process.env.REACT_APP_flightcreateorderAMADEUS_API_KEY,
+    secret: process.env.REACT_APP_flightcreateorderAMADEUS_API_SECRET,
+  },
+};
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+export default API_KEYS;
