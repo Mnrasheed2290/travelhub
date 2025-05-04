dcdf70c724f5e9c6ba2f3099d05b82f6ca278828
Mnrasheed2290 <naim.maria2@gmail.com>
2025-05-04 11:39:18 -0500

    Create Banner.js

---
+++ client/src/components/Banner.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../index.css'; // or './Banner.css' if separated
+
+const Banner = () => {
+  return (
+    <div className="banner-container">
+      <div className="banner-text">
+        Discover Unforgettable Flights & Hotels<br />with a Purpose.
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
