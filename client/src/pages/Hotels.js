f4f8899b7b9bc1a73bd3e790dbf1c71ad2753a8f
Mnrasheed2290 <naim.maria2@gmail.com>
2025-05-04 12:40:22 -0500

    Update Hotels.js

--- client/src/pages/Hotels.js
+++ client/src/pages/Hotels.js
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { useState } from "react";
 
-const Hotels = () => (
-  <div>
-    <h2>Search Hotels</h2>
-    {/* Hotel search form goes here */}
-  </div>
-);
+const hotelCities = [
+  "New York", "London", "Paris", "Istanbul", "Dubai", "Doha", "Kuala Lumpur"
+  // Israel excluded
+];
+
+function Hotels() {
+  const [city, setCity] = useState("");
+
+  return (
+    <div>
+      <h2>Search Hotels</h2>
+      <label>City:</label>
+      <select value={city} onChange={(e) => setCity(e.target.value)}>
+        <option value="">Select City</option>
+        {hotelCities.map(city => <option key={city} value={city}>{city}</option>)}
+      </select>
+
+      <button>Search Hotels</button>
+    </div>
+  );
+}
 
 export default Hotels;
