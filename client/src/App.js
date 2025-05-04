11985a800620a3eb9b8f1960352f1460d9dfb9f2
Mnrasheed2290 <naim.maria2@gmail.com>
2025-05-04 11:12:06 -0500

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,18 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import FlightSearch from "./pages/FlightSearch";
-import FlightBook from "./pages/FlightBook";
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import NavBar from './components/NavBar';
+import Home from './pages/Home';
+import Flights from './pages/Flights';
+import Hotels from './pages/Hotels';
 
 function App() {
   return (
     <Router>
+      <NavBar />
       <Routes>
-        <Route path="/" element={<FlightSearch />} />
-        <Route path="/book-flight" element={<FlightBook />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/flights" element={<Flights />} />
+        <Route path="/hotels" element={<Hotels />} />
       </Routes>
     </Router>
   );
