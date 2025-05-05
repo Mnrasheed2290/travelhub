a08a0f2ee417b5a09d85253db6faf85236e483ae
Mnrasheed2290 <naim.maria2@gmail.com>
2025-05-05 15:48:38 -0500

    Update Home.js

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -7,10 +7,8 @@ const HomePage = () => {
   return (
     <div className="home-page">
       <div className="hero-banner">
-        <img
-          src={bannerImage}
-          alt="Fly high with our deals"
-          className="hero-image"
+        <img src="/assets/banner.png" alt="Fly high with our deals" className="hero-image" />
+
         />
         <div className="hero-text">
           <h1>Discover the World</h1>
