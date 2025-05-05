57eaa834c4f552678d9146c7942ef84f446a3185
Mnrasheed2290 <naim.maria2@gmail.com>
2025-05-05 18:05:56 -0500

    Update Home.js

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -7,10 +7,10 @@ const HomePage = () => {
     <div className="home-page">
       <div className="hero-banner">
         <img
-          src="https://github.com/Mnrasheed2290/travelhub/blob/main/client/public/banner.png"
-          alt="Fly high with our deals"
-          className="hero-image"
-        />
+  src="/banner.png"
+  alt="Fly high with our deals"
+  className="hero-image"
+/>
         <div className="hero-text">
           <h1>Discover the World</h1>
           <p>Book flights and hotels with unbeatable prices</p>
