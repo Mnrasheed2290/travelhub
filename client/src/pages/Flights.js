14b41a4773e870fe2bc2286a9bec29dcabeb8efb
Mnrasheed2290 <naim.maria2@gmail.com>
2025-05-04 19:12:41 -0500

    Update Flights.js

--- client/src/pages/Flights.js
+++ client/src/pages/Flights.js
@@ -1,13 +1,20 @@
-import React, { useState } from "react";
+// File: client/src/pages/Flights.js
 
-const locations = [
-  "New York", "London", "Paris", "Dubai", "Tokyo", "Lahore", "Istanbul", "Cairo", "Johannesburg"
-  // Israel is intentionally excluded
-];
+import React, { useState, useEffect } from "react";
+import { fetchCitiesExcludingIsrael } from "../utils/amadeusAPI";
 
 function Flights() {
   const [origin, setOrigin] = useState("");
   const [destination, setDestination] = useState("");
+  const [locations, setLocations] = useState([]);
+
+  useEffect(() => {
+    const loadCities = async () => {
+      const cities = await fetchCitiesExcludingIsrael("a"); // You can loop or auto-expand this
+      setLocations(cities);
+    };
+    loadCities();
+  }, []);
 
   return (
     <div>
@@ -15,13 +22,17 @@ function Flights() {
       <label>From:</label>
       <select value={origin} onChange={(e) => setOrigin(e.target.value)}>
         <option value="">Select Origin</option>
-        {locations.map(loc => <option key={loc} value={loc}>{loc}</option>)}
+        {locations.map((loc) => (
+          <option key={loc.id} value={loc.name}>{loc.name}</option>
+        ))}
       </select>
 
       <label>To:</label>
       <select value={destination} onChange={(e) => setDestination(e.target.value)}>
         <option value="">Select Destination</option>
-        {locations.map(loc => <option key={loc} value={loc}>{loc}</option>)}
+        {locations.map((loc) => (
+          <option key={loc.id} value={loc.name}>{loc.name}</option>
+        ))}
       </select>
 
       <button>Search Flights</button>
