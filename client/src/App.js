18b51ec51d3f7d10608bcf96faee33def1b6e9c5
Mnrasheed2290 <naim.maria2@gmail.com>
2025-05-06 03:20:35 -0500

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import HomePage from "./pages/HomePage";
+import HomePage from "./pages/HomePage.css";
 import FlightSearch from "./components/FlightSearch";
 import FlightBook from "./components/FlightBook";
 import NavBar from "./components/NavBar"; // Optional if you have a NavBar component
