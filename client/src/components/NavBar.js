e01bf7fd5f03b907de9a170fa6d1aa3ad46f5464
Mnrasheed2290 <naim.maria2@gmail.com>
2025-05-07 18:02:17 -0500

    Update NavBar.js

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,12 +1,8 @@
+// File: client/src/components/NavBar.js
+
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './NavBar.css';
-
-const countries = [
-  'United States', 'Canada', 'United Kingdom', 'France', 'Germany', 'Australia', 'Japan', 'Turkey',
-  'Malaysia', 'Indonesia', 'United Arab Emirates', 'Morocco', 'South Africa'
-  // NOTE: Israel is excluded.
-];
 
 const NavBar = () => {
   return (
@@ -16,13 +12,7 @@ const NavBar = () => {
         <li><Link to="/">Home</Link></li>
         <li><Link to="/flights">Flights</Link></li>
         <li><Link to="/hotels">Hotels</Link></li>
-        <li>
-          <select>
-            {countries.map(country => (
-              <option key={country} value={country}>{country}</option>
-            ))}
-          </select>
-        </li>
+        <li><Link to="/book-flight">My Bookings</Link></li>
       </ul>
     </nav>
   );
