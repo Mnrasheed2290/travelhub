6f5dac08a62d3082c352f9d8c40f77e0e3b96378
Mnrasheed2290 <naim.maria2@gmail.com>
2025-05-09 13:41:49 -0500

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,17 @@
 // File: client/src/App.js
-
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import "./pages/HomePage.css";
 import HomePage from "./pages/HomePage";
-import FlightSearch from "./pages/FlightSearch";
+import FlightSearch from "./components/FlightSearch";
 import FlightBook from "./pages/FlightBook";
-import Hotels from "./pages/Hotels";
+import CarRental from "./pages/CarRental";
+import HotelSearch from "./pages/HotelSearch";
+import BookingHistory from "./pages/BookingHistory";
+import ChatSupport from "./pages/ChatSupport";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
 import NavBar from "./components/NavBar";
+import "./App.css";
 
 function App() {
   return (
@@ -18,7 +22,12 @@ function App() {
           <Route path="/" element={<HomePage />} />
           <Route path="/flights" element={<FlightSearch />} />
           <Route path="/book-flight" element={<FlightBook />} />
-          <Route path="/hotels" element={<Hotels />} />
+          <Route path="/cars" element={<CarRental />} />
+          <Route path="/hotels" element={<HotelSearch />} />
+          <Route path="/bookings" element={<BookingHistory />} />
+          <Route path="/chat" element={<ChatSupport />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
       </div>
     </Router>
