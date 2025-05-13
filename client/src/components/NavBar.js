570dee46a82eee9c3190651b714b8af1f2c11fb9
Mnrasheed2290 <naim.maria2@gmail.com>
2025-05-13 12:41:56 -0500

    Update NavBar.js

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,5 +1,4 @@
 // File: client/src/components/NavBar.js
-
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './NavBar.css';
@@ -7,12 +6,15 @@ import './NavBar.css';
 const NavBar = () => {
   return (
     <nav className="navbar">
-      <div className="navbar-title">SmartTravelHub</div>
-      <ul className="navbar-links">
-        <li><Link to="/">Home</Link></li>
+      <div className="logo">
+        <Link to="/">SkyNest</Link>
+      </div>
+      <ul className="nav-links">
         <li><Link to="/flights">Flights</Link></li>
         <li><Link to="/hotels">Hotels</Link></li>
-        <li><Link to="/book-flight">My Bookings</Link></li>
+        <li><Link to="/cars">Cars</Link></li>
+        <li><Link to="/bookings">My Trips</Link></li>
+        <li><Link to="/login">Login</Link></li>
       </ul>
     </nav>
   );
