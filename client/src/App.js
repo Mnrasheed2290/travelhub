99128acf1799e8c4115e0e6a3ab674fe9d521117
Mnrasheed2290 <naim.maria2@gmail.com>
2025-05-14 13:33:42 -0500

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,9 @@
-// File: client/src/App.js
-
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import FlightSearch from "./components/FlightSearch";
 import FlightBook from "./pages/FlightBook";
 import CarRental from "./pages/CarRentals";
-import Hotels from "./pages/Hotels";
 import HotelSearch from "./pages/HotelSearch";
 import BookingHistory from "./pages/BookingHistory";
 import ChatSupport from "./pages/ChatSupport";
@@ -20,39 +17,40 @@ import "./App.css";
 
 function App() {
   return (
-    <Router>
-      <div className="app">
-        <NavBar />
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/flights" element={<FlightSearch />} />
-          <Route path="/book-flight" element={<FlightBook />} />
-          <Route path="/cars" element={<CarRental />} />
-          <Route path="/hotels" element={<Hotels />} />
-          <Route path="/hotel-search" element={<HotelSearch />} />
-          <Route path="/bookings" element={<BookingHistory />} />
-          <Route path="/chat" element={<ChatSupport />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/checkout" element={<Checkout />} />
-          <Route path="/confirmation" element={<Confirmation />} />
-          <Route
-            path="*"
-            element={
-              <div className="not-found">
-                <h2>404 – Page Not Found</h2>
-                <p>The page you’re looking for doesn’t exist.</p>
-              </div>
-            }
-          />
-        </Routes>
+    <BookingProvider>
+      <Router>
+        <div className="app">
+          <NavBar />
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/flights" element={<FlightSearch />} />
+            <Route path="/book-flight" element={<FlightBook />} />
+            <Route path="/cars" element={<CarRental />} />
+            <Route path="/hotels" element={<HotelSearch />} />
+            <Route path="/bookings" element={<BookingHistory />} />
+            <Route path="/chat" element={<ChatSupport />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/checkout" element={<Checkout />} />
+            <Route path="/confirmation" element={<Confirmation />} />
+            <Route
+              path="*"
+              element={
+                <div className="not-found">
+                  <h2>404 – Page Not Found</h2>
+                  <p>The page you’re looking for doesn’t exist.</p>
+                </div>
+              }
+            />
+          </Routes>
 
-        {/* Floating chat button */}
-        <a href="/chat" className="chat-float" title="Live Chat">
-          💬
-        </a>
-      </div>
-    </Router>
+          {/* Floating chat button */}
+          <a href="/chat" className="chat-float" title="Live Chat">
+            💬
+          </a>
+        </div>
+      </Router>
+    </BookingProvider>
   );
 }
 
