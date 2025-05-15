3a60ebd0db6fb5aa85ab9bf8d8f491c7213a193b
Mnrasheed2290 <naim.maria2@gmail.com>
2025-05-15 11:39:36 -0500

    Update FlightSearch.js

--- client/src/components/FlightSearch.js
+++ client/src/components/FlightSearch.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 // ... (other imports, e.g., BookingContext, styling, etc.)
+import axios from "axios";
 
 function FlightSearch() {
   const navigate = useNavigate();
